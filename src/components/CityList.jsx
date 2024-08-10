0d32c04d0f5b11d296f674e45c3dacfd0b8e4dfe
Syedsafwan24 <136835578+Syedsafwan24@users.noreply.github.com>
2024-08-10 19:57:50 +0530

    initial commit

---
+++ src/components/CityList.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import CItyitem from "./CItyitem";
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return <Message message="Add your first city by clicking on map" />;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CItyitem key={city.id} city={city} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
