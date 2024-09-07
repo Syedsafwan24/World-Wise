a088a4907286cd51cadc7cde9955750a2f2df341
Syedsafwan24 <136835578+Syedsafwan24@users.noreply.github.com>
2024-09-07 18:18:30 +0530

    Passed Props Using Context

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:9000";
 const CitiesContext = createContext();
@@ -35,5 +36,11 @@ function CitiesProvider({ children }) {
     </CitiesContext.Provider>
   );
 }
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
+}
 
-export { CitiesProvider };
+export { CitiesProvider, useCities };
