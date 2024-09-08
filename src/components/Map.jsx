8ed2b73da4c83aebd417bb2bb017608d9c413b6e
Syedsafwan24 <136835578+Syedsafwan24@users.noreply.github.com>
2024-09-08 12:41:28 +0530

    Added leaflet map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,27 +1,48 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, Popup, Marker, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 import styles from "./Map.module.css";
+import { useState } from "react";
+
+// Fix for missing marker icons
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function Map() {
   const navigate = useNavigate();
 
+  const [mapPosition, setMapPosition] = useState([7, 3]);
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
-    >
-      Map
-      <h1>
-        Position : {lat},{lng}
-      </h1>
-      <button onClick={() => setSearchParams({ lat: 23, lng: 24 })}>
-        change Loc
-      </button>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
